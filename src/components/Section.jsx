6ad2a7f7432e9460e4a2d859ebf6db922ef7f5ee
icexop <williammagnus889@gmail.com>
2023-03-13 11:48:37 +0630

    articleAndAbout

--- src/components/Section.jsx
+++ src/components/Section.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
 import pic1 from '../assets/pic1.png'
+import About from './About'
+import Article from './Article'
 
 const Section = () => {
   return (
-    <div className='flex flex-col sm:flex-row'>
-        <div className='space-y-8 sm:mx-[150px] mx-4 mt-[100px] '>
-            <h1 className='text-3xl  sm:text-5xl sm:text-left font-bold text-[#013B52] text-center'>Connecting you to <br/>mental health <br/>support</h1>
-            <p className='sm:text-lg sm:text-left text-[#013B52] text-center text-sm'>We connect those suffer from metal health porblems to <br/>
-            effective help-quickly and confidentially to <br/>
-            everyone living in Myanmar.</p>
-            <div className='sm:justify-start space-x-7 flex space-x-2 justify-center'>
-                <button className='border-2 border-[#007E46] 
-                rounded-full text-sm px-3 py-2 sm:px-6 sm:py-3 text-[#007E46] font-semibold'>Get support</button>
-                <button className='border-2 border-[#013B52] 
-                rounded-full text-sm px-3 py-2 sm:px-6 sm:py-3 text-[#013B52] font-semibold'>About us</button>
-            </div>
-        </div>
-        <div className='flex justify-center'>
-            <img className='max-w-[300px] sm:max-w-[600px]' src={pic1} alt="Chatting_image" />
-        </div>
+    <div className='flex flex-col space-y-20 overflow-x-hidden mx-10'>
+      <div className='flex flex-col md:flex-row justify-around items-center mt-[40px] md:mx-10'>
+          <div className='space-y-8 ml-4 items-left flex flex-col'>
+              <h1 className='text-3xl font-bold text-[#013B52] text-center md:text-4xl md:text-left lg:text-6xl'>Connecting you to <br/>mental health <br/>support</h1>
+              <p className=' text-[#013B52] text-center text-sm md:text-left md:text-[15px] lg:text-base'>We connect those suffer from metal health porblems to <br/>
+              effective help-quickly and confidentially to <br/>
+              everyone living in Myanmar.</p>
+              <div className='lg:justify-start md:items-start md:flex-col md:space-y-3 md:space-x-0 items-left lg:items-center lg:flex-row lg:space-y-0 lg:space-x-7 space-x-7 flex space-x-2 justify-center'>
+                  <button className='border-2 border-[#007E46] 
+                  rounded-full text-sm px-3 py-2 text-[#007E46] font-semibold px-5 lg:py-3 text-sm xl:px-6 xl:py-3 xl:text-base'>Get support</button>
+                  <button className='border-2 border-[#013B52] 
+                  rounded-full text-sm px-3 py-2 text-[#013B52] font-semibold px-8 lg:py-3 text-sm xl:px-9 xl:py-3 xl:text-base'>About us</button>
+              </div>
+          </div>
+          <div className='flex justify-center'>
+              <img className='max-w-[350px] md:max-w-[500px] lg:max-w-[550px] xl:max-w-[600px]' src={pic1} alt="Chatting_image" />
+          </div>
+      </div>
+      <div className='h-[1.5px] w-full bg-gray-300'></div>
+      <Article></Article>
+      <About></About>
     </div>
   )
 }
