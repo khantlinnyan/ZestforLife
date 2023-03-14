4aad897bdffb194aa717ac5f8339952e505b7ad5
khantlinnyan <khantlinnyan011027@gmail.com>
2023-03-14 09:50:12 +0630

    contact

--- src/pages/GetSupport.jsx
+++ src/pages/GetSupport.jsx
@@ -1,54 +1,58 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import { useLocation } from "react-router-dom";
+import Contact from "../components/Contact";
 
 const GetSupport = () => {
   const location = useLocation();
-  console.log(location.state);
+
   return (
-    <div className="py-2 px-4 lg:px-40 mt-3">
-      <div>
-        {location.state ? (
-          <>
-          <div
-            style={{
-              background: location.state.color,
-              color: location.state.titleColor,
-            }}
-            className="py-2 px-4 rounded-lg"
-          >
-            Result
-            <h1 style={{ color: location.state.textColor }}>
-              {location.state.result}
-            </h1>
-          </div>
-          <div className="mt-5 ">
-          <p className="inline text-lg text-gray-700">Mental health check again - </p>
-          <Link
-            to="/getsupport/mentalcheck"
-            className="text-blue-800 under mt-4 text-lg"
-          >
-            Click here
-          </Link>
+    <>
+      <div className="py-2 px-4 lg:px-40 mt-3">
+        <div>
+          {location.state ? (
+            <>
+              <div
+                style={{
+                  background: location.state.color,
+                  color: location.state.titleColor,
+                }}
+                className="py-2 px-4 rounded-lg text-xl"
+              >
+                Result
+                <h1 style={{ color: location.state.textColor }}>
+                  {location.state.result}
+                </h1>
+              </div>
+              <div className="mt-5 ">
+                <p className="inline text-lg text-gray-700">
+                  Mental health check again -{" "}
+                </p>
+                <Link
+                  to="/getsupport/mentalcheck"
+                  className="text-blue-800 under mt-4 text-lg"
+                >
+                  Click here
+                </Link>
+              </div>
+            </>
+          ) : (
+            <div className="mt-2 bg-gray-100 px-4 py-2 mb-10 rounded">
+            <p className="inline text-lg font-semibold text-gray-700">
+                Mental health check
+              </p>
+              <Link
+                to="/getsupport/mentalcheck"
+                className="text-blue-800 block under mt-4 text-lg"
+              >
+                Click here
+              </Link>
+            </div>
+          )}
         </div>
-          </>
-        ) : (
-          <div className="mt-5 ">
-            <p className="inline text-lg text-gray-700">
-              Mental health check-{" "}
-            </p>
-            <Link
-              to="/getsupport/mentalcheck"
-              className="text-blue-800 under mt-4 text-lg"
-            >
-              Click here
-            </Link>
-          </div>
-        )}
-        
-        <h3></h3>
+        <Contact/>
       </div>
-    </div>
+    </>
   );
 };
 
