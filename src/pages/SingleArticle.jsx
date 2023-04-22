7eab5229afd4d3210c142be8dc0aecca61b8ecab
Khant Lin Nyan <99181471+khantlinnyan@users.noreply.github.com>
2023-04-22 18:46:58 +0630

    2ndTesitingUpdateSingleArticle

--- src/pages/SingleArticle.jsx
+++ src/pages/SingleArticle.jsx
@@ -1,112 +1,118 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+
+import { useParams } from "react-router-dom";
 
 import supabase from "../config/superbaseClient";
 
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
 
-const Articlecomp = () => {
+import Loader from "../components/Loader";
 
-  const [articles, setArticles] = useState([]);
+const SingleArticle = () => {
 
-  const [isLoading, setIsLoading] = useState(true);
+  const [allData, setAllData] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(false);
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const { id } = useParams("");
+
+  const fetchData = async () => {
+
+    setIsLoading(true);
+
+    const { data, error } = await supabase
+
+      .from("articles")
+
+      .select("*")
+
+      .eq("id", id);
+
+    if (error) {
+
+      setErrorMessage("Could not connect");
+
+    } else {
+
+      setAllData(data);
+
+    }
+
+    setIsLoading(false);
+
+  };
 
   useEffect(() => {
 
-    const fetchArticles = async () => {
-
-      const { data, error } = await supabase.from("articles").select("*");
-
-      if (data) {
-
-        setArticles(data);
-
-        setIsLoading(false);
-
-      }
-
-    };
-
-    fetchArticles();
+    fetchData();
 
   }, []);
 
+  if (isLoading) {
+
+    return <Loader />;
+
+  }
+
+  if (errorMessage) {
+
+    return <div className="text-red-500">{errorMessage}</div>;
+
+  }
+
   return (
 
-    <div className="flex flex-col items-center mt-3">
+    <div className="mb-10">
 
-      <div className="flex flex-col items-center space-y-2 mt-6">
+      {allData.map((data) => {
 
-        <h1 className="text-4xl font-bold text-green-500">Articles</h1>
+        return (
 
-      </div>
+          <div className="px-7 mt-3 font-semibold text-gray-700" key={data.id}>
 
-      {isLoading ? (
+            <img
 
-        <div className="p-10 flex items-center justify-center">
+              className="mt-3 lg:px-32 md:px-16 rounded-md"
 
-          <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900"></div>
+              src={data.img}
 
-        </div>
+              alt={data.title}
 
-      ) : (
+            />
 
-        <div className="p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+            <div className="mt-5 lg:px-32 md:px-16">
 
-          {articles.map((article) => (
+              <h3 className="text-3xl">{data.title}</h3>
 
-            <div key={article.id} className="bg-white rounded-lg overflow-hidden shadow-md">
+              <span className="inline-block mt-2 bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
 
-              <div className="relative">
+                {`#${data.category}`}
 
-                <img
+              </span>
 
-                  className="w-full h-48 object-cover"
+              <p className="mt-2 text font-normal">
 
-                  src={article.img}
+                Original Post:{" "}
 
-                  alt={article.title}
+                <a className="underline text-green-500" href={data.orglink}>
 
-                />
+                  Click here
 
-                <span className="absolute top-0 left-0 bg-blue-500 text-white font-bold py-1 px-2 rounded-md ml-4 mt-4 text-sm">
+                </a>
 
-                  {article.category}
+              </p>
 
-                </span>
-
-              </div>
-
-              <div className="p-4">
-
-                <h2 className="font-bold text-lg">{article.title}</h2>
-
-                <p className="mt-2 text-gray-600">{article.description}</p>
-
-              </div>
-
-              <div className="p-4 bg-gray-100 border-t border-gray-200">
-
-                <Link
-
-                  to={`/articles/${article.id}`}
-
-                  className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-md text-sm inline-block"
-
-                >
-
-                  Read Article
-
-                </Link>
-
-              </div>
+              <p className="text-lg font-normal mt-5">{data.para}</p>
 
             </div>
 
-          ))}
+          </div>
 
-        </div>
+        );
 
-      )}
+      })}
 
     </div>
 
@@ -114,4 +120,5 @@ const Articlecomp = () => {
 
 };
 
-export default Articlecomp;
+export default SingleArticle;
+
