68797bef53962707c974b4cca0f8406f87948e3f
Khant Lin Nyan <99181471+khantlinnyan@users.noreply.github.com>
2023-04-22 18:31:01 +0630

    TestingUpdateSingleArticle

--- src/pages/SingleArticle.jsx
+++ src/pages/SingleArticle.jsx
@@ -1,50 +1,117 @@
-import React from "react";
-import { useParams } from "react-router-dom";
+import React, { useState, useEffect } from "react";
+
 import supabase from "../config/superbaseClient";
-import { useState, useEffect } from "react";
 
-const SingleArticle = () => {
-  const [allData, setAllData] = useState([]);
-  const { id } = useParams("");
-  const fetchData = async () => {
-    const { data, error } = await supabase
-      .from("articles")
-      .select("*")
-      .eq("id", id);
+import { Link } from "react-router-dom";
 
-    if (error) {
-      setAllData("Could not connect");
-    }
-    setAllData(data);
-  };
+const Articlecomp = () => {
+
+  const [articles, setArticles] = useState([]);
+
+  const [isLoading, setIsLoading] = useState(true);
+
   useEffect(() => {
-    fetchData();
+
+    const fetchArticles = async () => {
+
+      const { data, error } = await supabase.from("articles").select("*");
+
+      if (data) {
+
+        setArticles(data);
+
+        setIsLoading(false);
+
+      }
+
+    };
+
+    fetchArticles();
+
   }, []);
+
   return (
-    <div className="mb-10">
-      {allData.map((data) => {
-        return (
-          <div className="px-7 mt-3 font-semibold text-gray-70" key={data.id}>
-          <img className="mt-3 lg:px-32 md:px-16 rounded-md" src={data.img}/>
-            
-            <div className="mt-5 lg:px-32 md:px-16">
-              <h3 className="text-2xl">{data.title}</h3>
-              <span className="inline-block mt-2 bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2] mb-2">
-                  {`#${data.category}`}
+
+    <div className="flex flex-col items-center mt-3">
+
+      <div className="flex flex-col items-center space-y-2 mt-6">
+
+        <h1 className="text-4xl font-bold text-green-500">Articles</h1>
+
+      </div>
+
+      {isLoading ? (
+
+        <div className="p-10 flex items-center justify-center">
+
+          <div className="animate-spin rounded-full h-16 w-16 border-t-2 border-b-2 border-gray-900"></div>
+
+        </div>
+
+      ) : (
+
+        <div className="p-10 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8">
+
+          {articles.map((article) => (
+
+            <div key={article.id} className="bg-white rounded-lg overflow-hidden shadow-md">
+
+              <div className="relative">
+
+                <img
+
+                  className="w-full h-48 object-cover"
+
+                  src={article.img}
+
+                  alt={article.title}
+
+                />
+
+                <span className="absolute top-0 left-0 bg-blue-500 text-white font-bold py-1 px-2 rounded-md ml-4 mt-4 text-sm">
+
+                  {article.category}
+
                 </span>
-              <p className="mt-2 text font-normal">
-                Original Post:{" "}
-                <a className="underline text-green" href={data.orglink}>
-                  Click here
-                </a>
-              </p>
-              <p className="text-base font-normal mt-5">{data.para}</p>
+
+              </div>
+
+              <div className="p-4">
+
+                <h2 className="font-bold text-lg">{article.title}</h2>
+
+                <p className="mt-2 text-gray-600">{article.description}</p>
+
+              </div>
+
+              <div className="p-4 bg-gray-100 border-t border-gray-200">
+
+                <Link
+
+                  to={`/articles/${article.id}`}
+
+                  className="bg-green-500 hover:bg-green-600 text-white py-2 px-4 rounded-md text-sm inline-block"
+
+                >
+
+                  Read Article
+
+                </Link>
+
+              </div>
+
             </div>
-          </div>
-        );
-      })}
+
+          ))}
+
+        </div>
+
+      )}
+
     </div>
+
   );
+
 };
 
-export default SingleArticle;
+export default Articlecomp;
