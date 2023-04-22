3c3e2167ffd786cdc3c86d8cd50435e147a23c3e
Khant Lin Nyan <99181471+khantlinnyan@users.noreply.github.com>
2023-04-22 19:18:34 +0630

    7thTestingUpdateSingleArticle

--- src/pages/SingleArticle.jsx
+++ src/pages/SingleArticle.jsx
@@ -1,5 +1,7 @@
 import React from "react";
-import Loader from "../components/Loader;
+
+import Loader from "../components/Loader";
+
 import { useParams } from "react-router-dom";
 
 import supabase from "../config/superbaseClient";
