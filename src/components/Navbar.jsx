c3119cd37a7fceb0346d96ea233dc1113eecfcb3
Khant Lin Nyan <99181471+khantlinnyan@users.noreply.github.com>
2023-05-04 01:20:49 +0630

    4thUpdateNavbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,175 +1,153 @@
 import { useState } from "react";
 
-import { Link } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 import { Navtitle } from "./Navbartitle";
 
-export default function Navbar() {
+export default function Navigation() {
 
   const [navbar, setNavbar] = useState(false);
 
-  const handleToggle = () => {
-
-    setNavbar((prev) => !prev);
-
-  };
+  const isMobile = window.innerWidth <= 640; // Check if screen size is smaller than 640px
 
   return (
 
-    <nav className="sticky top-0 shadow-md bg-white z-50">
+    <>
 
-      <div className="container mx-auto px-4">
+      {/* Bottom navigation for mobile */}
 
-        <div className="flex justify-between items-center py-4 md:py-6">
+      {isMobile && (
 
-          <Link to="/">
+        <nav className="fixed bottom-0 left-0 right-0 z-50 bg-white border-t-2 border-gray-300">
 
-            <h1 className="text-lg md:text-2xl font-bold text-green-500">
+          <ul className="flex justify-between items-center py-2">
 
-              ZestforLife
+            {Navtitle.map((i) => (
 
-            </h1>
+              <li key={i.id}>
 
-          </Link>
+                <Link
 
-          <div className="md:hidden">
+                  to={i.to}
 
-            <button
-
-              className="p-2 rounded-md text-gray-700 hover:bg-gray-100 focus:outline-none focus:bg-gray-100"
-
-              onClick={handleToggle}
-
-            >
-
-              {navbar ? (
-
-                <svg
-
-                  xmlns="http://www.w3.org/2000/svg"
-
-                  className="w-6 h-6"
-
-                  viewBox="0 0 20 20"
-
-                  fill="currentColor"
+                  className="flex flex-col items-center justify-center text-xs text-gray-500 hover:text-green-500"
 
                 >
 
-                  <path
+                  <span className="mb-1">{i.icon}</span>
 
-                    fillRule="evenodd"
+                  {i.name}
 
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                </Link>
 
-                    clipRule="evenodd"
+              </li>
 
-                  />
+            ))}
 
-                </svg>
+          </ul>
 
-              ) : (
+        </nav>
 
-                <svg
+      )}
 
-                  xmlns="http://www.w3.org/2000/svg"
+      {/* Top navigation for desktop */}
 
-                  className="w-6 h-6"
+      <nav className="hidden lg:block w-full shadow sticky top-0 overflow-hidden bg-white opacity-90 z-40">
 
-                  fill="none"
+        <div className="justify-between px-4 mx-auto lg:max-w-7xl md:items-center md:flex md:px-8">
 
-                  viewBox="0 0 24 24"
+          <div>
 
-                  stroke="currentColor"
+            <div className="flex items-center justify-between py-3 md:py-5 md:block">
 
-                  strokeWidth={2}
+              <Link to={'/'}>
+
+                <h2 className="text-xl md:text-3xl font-bold text-green-500">
+
+                  ZestforLife
+
+                </h2>
+
+              </Link>
+
+              <div className="md:hidden">
+
+                <button
+
+                  className="p-2 text-gray-700 rounded-md outline-none focus:border-gray-400 focus:border"
+
+                  onClick={() => setNavbar(!navbar)}
 
                 >
 
-                  <path
+                  {navbar ? (
 
-                    strokeLinecap="round"
+                    <svg
 
-                    strokeLinejoin="round"
+                      xmlns="http://www.w3.org/2000/svg"
 
-                    d="M4 6h16M4 12h16M4 18h16"
+                      className="w-6 h-6"
 
-                  />
+                      viewBox="0 0 20 20"
 
-                </svg>
+                      fill="currentColor"
 
-              )}
+                    >
 
-            </button>
+                      <path
+
+                        fillRule="evenodd"
+
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+
+                        clipRule="evenodd"
+
+                      />
+
+                    </svg>
+
+                  ) : (
+
+                    <svg
+
+                      xmlns="http://www.w3.org/2000/svg"
+
+                      className="w-6 h-6"
+
+                      fill="none"
+
+                      viewBox="0 0 24 24"
+
+                      stroke="currentColor"
+
+                      strokeWidth={2}
+
+                    >
+
+                      <path
+
+                        strokeLinecap="round"
+
+                        strokeLinejoin="round"
+
+                        d="M4 6h16M4 12h16M4 18h16"
+
+                      />
+
+                    </svg>
+
+                  )}
+
+                </button>
+
+              </div>
+
+            </div>
 
           </div>
 
-          <div className="hidden md:block">
-
-            <ul className="flex space-x-6 items-center">
-
-              {Navtitle.map((item) => (
-
-                <li key={item.id}>
-
-                  <Link
-
-                    className="text-base md:text-lg font-medium text-[#013B52] hover:text-green-500"
-
-                    to={item.to}
-
-                  >
-
-                    {item.name}
-
-                  </Link>
-
-                </li>
-
-              ))}
-
-            </ul>
-
           </div>
-
-        </div>
-
-        <div
-
-          className={`md:hidden ${
-
-            navbar ? "flex flex-col" : "hidden"
-
-          } space-y-4`}
-
-        >
-
-          {Navtitle.map((item) => (
-
-            <Link
-
-              key={item.id}
-
-              className="text-base font-medium text-[#013B52] hover:text-green-500"
-
-              to={item.to}
-
-              onClick={handleToggle}
-
-            >
-
-              {item.name}
-
-            </Link>
-
-          ))}
-
-        </div>
-
-      </div>
-
-    </nav>
-
-  );
-
+</nav>
+</>
+);
 }
